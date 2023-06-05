debfe2dbc8abd1fd7d0877fb480dc3f612cff5cf
VaibsDev <devadiga08vaibhav@gmail.com>
2023-06-05 12:46:07 +0530

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool isWalking;
+    // public float moveSpeed;
+    [SerializeField]private float moveSpeed =10f;
+    [SerializeField]private float rotateSpeed;
+    // Update is called once per frame
+    private void Update()
+    {
+        CharMove();
+    }
+    void CharMove()
+    {
+        Vector2 inputVector= new Vector2(0,0);
+        if(Input.GetKey(KeyCode.W))
+        {
+            inputVector.y =+1;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            inputVector.x =-1;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            inputVector.y =-1;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            inputVector.x =+1;
+        }
+        inputVector = inputVector.normalized;
+        Vector3 movDir= new Vector3(inputVector.x,0f,inputVector.y);
+        // transform.position += (Vector3)inputVector; 
+        transform.position += movDir*moveSpeed* Time.deltaTime; 
+        isWalking = movDir!=Vector3.zero;
+        // Debug.Log(Time.deltaTime);
+        transform.forward =Vector3.Slerp(transform.forward,movDir,rotateSpeed*Time.deltaTime) ;
+    }
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    public Player player;
+    private const string IS_WALKING = "IsWalking";
+    private Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update() {
+        animator.SetBool(IS_WALKING,player.IsWalking());
+    }
+}
